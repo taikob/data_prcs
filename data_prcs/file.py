3962e12b89a05fc3600b1eea01f4941ca8521009
taikob <taisukekbys@gmail.com>
2021-02-18 13:01:01 +0900

    fix file_para

--- data_prcs/file.py
+++ data_prcs/file.py
@@ -14,7 +14,10 @@ def file_para(path,comtxt,title=None,paramnum=None):
             data = np.loadtxt(path + '/' + dir, dtype='float')
             if paramnum is None:
                 paramnum = len(tprm)
-            datanum=data.shape[0]
+            if data.shape is ():
+                datanum=1
+            else:
+                datanum=data.shape[0]
             break
 
     out = np.empty((0, paramnum + datanum))
@@ -22,7 +25,10 @@ def file_para(path,comtxt,title=None,paramnum=None):
         if comtxt in dir:
             data = np.loadtxt(path + '/' + dir, dtype='float')
             tprm = g.readtitleparam(dir,2)
-            tprm=tprm+data.tolist()
+            if datanum==1:
+                tprm=tprm+[data]
+            else:
+                tprm=tprm+data.tolist()
             out = np.append(out, np.array([tprm]), axis=0)
 
     if title is None: title='stat_data.csv'
